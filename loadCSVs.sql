c237033338f6132e123eecc591c8e67fd7dcaf84
Soumil Mukherjee <soumil0526@gmail.com>
2020-08-04 03:42:05 -0400

    Setup file to load CSVs into psql with schemas

---
+++ loadCSVs.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS public.library_link;
+CREATE TABLE public.library_link (
+    id integer NOT NULL,
+    value text NOT NULL
+);
+\copy public.library_link FROM 'links.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS public.library_tokenrecord;
+CREATE TABLE public.library_tokenrecord (
+    id integer NOT NULL,
+    token text NOT NULL,
+    source_id integer NOT NULL,
+    index integer NOT NULL
+);
+\copy public.library_tokenrecord FROM 'tokens.csv' DELIMITER ',' CSV HEADER;
+
+DROP TABLE IF EXISTS public.library_functionrecord;
+CREATE TABLE public.library_functionrecord (
+    id integer NOT NULL,
+    text text NOT NULL,
+    source_id integer NOT NULL,
+    index integer NOT NULL
+);
+\copy public.library_functionrecord FROM 'functions.csv' DELIMITER ',' CSV HEADER;
